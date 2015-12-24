b23b165ef18b640e6f6ead623247cd43b1f50947
Luemiu <root@luemiu.com>
2015-12-25 04:07:50 +0800

    insert event

--- client.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CLIENT_H__
-#define __CLIENT_H__
-
-extern void * accept_client(void *);
-extern void * opera_data(void *);
-extern void * close_client(void *);
-
-#endif
--- thread.h
+++
@@ -1,7 +0,0 @@
-#ifndef __THREAD_H__
-#define __THREAD_H__
-
-extern void * thread_accept(void *);
-extern void * thread_commut(void *);
-
-#endif
