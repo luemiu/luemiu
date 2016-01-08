8c07a03476ace7a73b3ee71c3b4ee5a62b9f16bb
Luemiu <root@luemiu.com>
2016-01-08 22:18:39 +0800

    IPv4 and IPv6

--- src/main.c
+++ src/main.c
@@ -4,121 +4,105 @@ LIST_HEAD(head);
 pthread_mutex_t head_mutex = PTHREAD_MUTEX_INITIALIZER;
 int main (int argc, char *argv[])  
 { 
-		if(optargs(argc, argv) & 1)
-				daemon(0, 0);
-		char port[] = "8008";
-		pthread_t ctid;//operation client data thread
-		openlog(APPNAME, LOG_ODELAY, LOG_LOCAL0);
-		if (signal(SIGSEGV, dump) == SIG_ERR)
-				syslog(LOG_DEBUG, "%s %s %d Dump SIG_ERR\n", __FILE__, __func__, __LINE__);
-		if( (sfd = bind_sock(port)) == -1)
-				return EXIT_FAILURE;
-		if(set_nonblocking(sfd) == -1)
-		{
-				syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, strerror(errno));
-				close(sfd);
-				return EXIT_FAILURE;
-		}
-		if(listen (sfd, SOMAXCONN) == -1)  
-		{
-				syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, strerror(errno));
-				close(sfd);
-				return EXIT_FAILURE;
-		}
-		if( (efd = epoll_create1 (0)) == -1)  
-		{
-				syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, strerror(errno));
-				close(sfd);
-				return EXIT_FAILURE;
-		}
-		event.data.fd = sfd;  
-		event.events = EPOLLIN | EPOLLET;
-		if(epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event) == -1)
-		{
-				syslog(LOG_ERR, "%s %s %d EPOLL_CTL_ADD %s\n", __FILE__, __func__, __LINE__, strerror(errno));
-				close(efd);
-				close(sfd);
-				return EXIT_FAILURE;
-		}
-		if( (events = (struct epoll_event *)MALLOC(MAXEVENTS*sizeof(event))) == NULL)
-		{
-				syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, "Can not calloc!");
-				close(efd);
-				close(sfd);
-				return EXIT_FAILURE;
-		}
-		int freeflag = 1;
-		while(1)  
-		{  
-				int n, i, result;  
-				int opflag = 0;
-				if( (n = epoll_wait(efd, events, MAXEVENTS, -1)) == -1)
-						if( (errno == EINTR) || (errno == EAGAIN) )
-								continue;
-				for(i = 0; i < n; i++)  
+				sockfd[0] = -1;
+				sockfd[1] = -1;
+				if(optargs(argc, argv) & 1)
+								daemon(0, 0);
+				char port[] = "8008";
+				pthread_t ctid;//operation client data thread
+				openlog(APPNAME, LOG_ODELAY, LOG_LOCAL0);
+				if (signal(SIGSEGV, dump) == SIG_ERR)
+								syslog(LOG_DEBUG, "%s %s %d Dump SIG_ERR\n", __FILE__, __func__, __LINE__);
+				if( (efd = epoll_create1 (0)) == -1)  
+				{
+								syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, strerror(errno));
+								return EXIT_FAILURE;
+				}
+				if(bind_sock(port) == -1)
+				{
+								close(efd);
+								return EXIT_FAILURE;
+				}
+				if( (events = (struct epoll_event *)MALLOC(MAXEVENTS*sizeof(event))) == NULL)
+				{
+								syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, "Can not calloc!");
+								close(efd);
+								return EXIT_FAILURE;
+				}
+				int freeflag = 1;
+				while(1)  
 				{  
-						if(events[i].data.fd == sfd)
-						{
-								if( (result = pthread_create(&ctid, NULL, accept_thread, NULL)) != 0)
+								int n, i, result;  
+								int opflag = 0;
+								if( (n = epoll_wait(efd, events, MAXEVENTS, -1)) == -1)
+												if( (errno == EINTR) || (errno == EAGAIN) )
+																continue;
+								for(i = 0; i < n; i++)  
+								{  
+												if((events[i].data.fd == sockfd[0]) || (events[i].data.fd == sockfd[1]))
+												{
+																if( (result = pthread_create(&ctid, NULL, accept_thread, (void*)events[i].data.fd)) != 0)
+																{
+																				syslog(LOG_ERR, "%s %s %d event[%d] accept thread create %s\n",
+																												__FILE__, __func__, __LINE__, n, strerror(result));
+																}
+																continue;
+												}
+												if( (events[i].events & EPOLLRDHUP) || (events[i].events & EPOLLHUP)
+																				|| (events[i].events & EPOLLERR) || (events[i].events & EPOLLPRI) )
+												{
+																continue;
+												}
+												if(events[i].events & EPOLLIN)
+												{
+																if( (result =pthread_mutex_lock(&head_mutex)) != 0)
+																				syslog(LOG_ERR, "%s %s %d Lock %s\n", 
+																												__FILE__, __func__, __LINE__, strerror(result));
+																struct client * sc = (struct client *)malloc(1*sizeof(struct client));
+																if(sc == NULL)
+																{
+																				syslog(LOG_ERR, "%s %s %d Malloc events[%d]\n", __FILE__, __func__, __LINE__, i);
+																				close(events[i].data.fd);
+																				if( (result = pthread_mutex_unlock(&head_mutex)) != 0)
+																								syslog(LOG_ERR, "%s %s %d Unlock %s\n", 
+																																__FILE__, __func__, __LINE__, strerror(result));
+																				continue;
+																}
+																bzero(sc, sizeof(struct client));
+																sc->cevent = events[i];
+																sc->readed = 0;
+																list_add_tail(&sc->list, &head);	
+																sc = NULL;
+																if( (result = pthread_mutex_unlock(&head_mutex)) != 0)
+																				syslog(LOG_ERR, "%s %s %d Unlock %s\n", 
+																												__FILE__, __func__, __LINE__, strerror(result));
+																opflag = 1;
+												}
+								} // end for
+								if(opflag)
 								{
-										syslog(LOG_ERR, "%s %s %d event[%d] accept thread create %s\n",
-														__FILE__, __func__, __LINE__, n, strerror(result));
+												if( (result = pthread_create(&ctid, NULL, commut_thread, NULL)) != 0)
+												{
+																syslog(LOG_DEBUG, "%s %s %d commut thread create %s, conntinue\n",
+																								__FILE__, __func__, __LINE__, strerror(result));
+																continue;
+												}
 								}
-								continue;
-						}
-						if( (events[i].events & EPOLLRDHUP) || (events[i].events & EPOLLHUP)
-										|| (events[i].events & EPOLLERR) || (events[i].events & EPOLLPRI) )
-						{
-								continue;
-						}
-						if(events[i].events & EPOLLIN)
-						{
-								if( (result =pthread_mutex_lock(&head_mutex)) != 0)
-										syslog(LOG_ERR, "%s %s %d Lock %s\n", 
-														__FILE__, __func__, __LINE__, strerror(result));
-								struct client * sc = (struct client *)malloc(1*sizeof(struct client));
-								if(sc == NULL)
+								if(freeflag)
 								{
-										syslog(LOG_ERR, "%s %s %d Malloc events[%d]\n", __FILE__, __func__, __LINE__, i);
-										close(events[i].data.fd);
-										if( (result = pthread_mutex_unlock(&head_mutex)) != 0)
-												syslog(LOG_ERR, "%s %s %d Unlock %s\n", 
-																__FILE__, __func__, __LINE__, strerror(result));
-										continue;
+												if( (result = pthread_create(&ctid, NULL, free_thread, NULL)) != 0)
+												{
+																syslog(LOG_DEBUG, "%s %s %d Free thread create %s\n",
+																								__FILE__, __func__, __LINE__, strerror(result));
+																freeflag = 1;
+												}
+												freeflag = 0;
 								}
-								bzero(sc, sizeof(struct client));
-								sc->cevent = events[i];
-								sc->readed = 0;
-								list_add_tail(&sc->list, &head);	
-								sc = NULL;
-								if( (result = pthread_mutex_unlock(&head_mutex)) != 0)
-										syslog(LOG_ERR, "%s %s %d Unlock %s\n", 
-														__FILE__, __func__, __LINE__, strerror(result));
-								opflag = 1;
-						}
-				} // end for
-				if(opflag)
-				{
-						if( (result = pthread_create(&ctid, NULL, commut_thread, NULL)) != 0)
-						{
-								syslog(LOG_DEBUG, "%s %s %d commut thread create %s, conntinue\n",
-												__FILE__, __func__, __LINE__, strerror(result));
-								continue;
-						}
-				}
-				if(freeflag)
-				{
-						if( (result = pthread_create(&ctid, NULL, free_thread, NULL)) != 0)
-						{
-								syslog(LOG_DEBUG, "%s %s %d Free thread create %s\n",
-												__FILE__, __func__, __LINE__, strerror(result));
-								freeflag = 1;
-						}
-						freeflag = 0;
-				}
-		} // end while 
-		free(events);  
-		close(sfd);  
-		closelog();
-		return EXIT_SUCCESS;  
+				} // end while 
+				free(events);  
+				close(sockfd[0]);  
+				close(sockfd[1]);  
+				close(efd);  
+				closelog();
+				return EXIT_SUCCESS;  
 }  
--- src/miu.c
+++ src/miu.c
@@ -2,228 +2,266 @@
 
 char * get_date_time(unsigned long threadid)
 {
-		static char str[128];
-		time_t t = time(NULL);
-		snprintf(str, sizeof str, "0x%lX %.24s", threadid, ctime(&t));
-		return str;
+				static char str[128];
+				time_t t = time(NULL);
+				snprintf(str, sizeof str, "0x%lX %.24s", threadid, ctime(&t));
+				return str;
 }
 
 void * Malloc(size_t size)
 {
 #if DEBUG
-		syslog(LOG_DEBUG, "%s %s %d Malloc init size[%d]\n", __FILE__, __func__, __LINE__, size);
+				syslog(LOG_DEBUG, "%s %s %d Malloc init size[%d]\n", __FILE__, __func__, __LINE__, size);
 #endif
-		return malloc(size);
+				return malloc(size);
 }
 
 void usage(void)
 {
-		fprintf(stdout, "Usage:%s [-d | -h]\n", APPNAME);
-		fprintf(stdout, "      -d Daemon init\n");
-		fprintf(stdout, "      -h Show this help message\n");
-		exit(0);
+				fprintf(stdout, "Usage:%s [-d | -h]\n", APPNAME);
+				fprintf(stdout, "      -d Daemon init\n");
+				fprintf(stdout, "      -h Show this help message\n");
+				exit(0);
 }
 
 int optargs(int argc, char ** argv)
 {
-		int ch;
-		int args = 0;
-		while ((ch = getopt(argc, argv, "dh")) != -1)
-				switch(ch)
-				{
-						case 'd':
-								args |= 1;
-								break;
-						case 'h':
-								usage();
-								break;
-						default:
-								usage();
-								break;
-				}
-		return args;
+				int ch;
+				int args = 0;
+				while ((ch = getopt(argc, argv, "dh")) != -1)
+								switch(ch)
+								{
+												case 'd':
+																args |= 1;
+																break;
+												case 'h':
+																usage();
+																break;
+												default:
+																usage();
+																break;
+								}
+				return args;
 }
 
 int bind_sock(char * port)
 {
-		struct addrinfo inaddr;
-		struct addrinfo * result, *rp;
+				struct addrinfo * result, *rp, hints;
+				int ret, yes = 1;
+				memset(&hints, 0, sizeof hints);
+				hints.ai_family = AF_UNSPEC;
+				hints.ai_socktype = SOCK_STREAM;
+				hints.ai_flags = AI_PASSIVE; 
+				if( (ret = getaddrinfo(NULL, port, &hints, &result)) != 0)
+				{
+								printf("getaddrinfo %s", gai_strerror(ret));
+								return -1;
+				}
+				ret = 0;
+				for(rp = result; rp != NULL; rp = rp->ai_next)
+				{
+								ret++;
+								if( (sockfd[ret-1] = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1)
+								{
+												printf("socket %s", gai_strerror(errno));
+												continue;
+								}
+								if (setsockopt(sockfd[ret-1], SOL_SOCKET, SO_REUSEPORT, &yes, sizeof(int)) == -1)
+								{
+												printf("setsockopt %s", gai_strerror(errno));
+												close(sockfd[ret-1]);
+												sockfd[ret-1] = -1;
+												continue;
+								}
+								if(bind(sockfd[ret-1], rp->ai_addr, rp->ai_addrlen) == -1)
+								{
+												printf("bind %s", gai_strerror(errno));
+												close(sockfd[ret-1]);
+												sockfd[ret-1] = -1;
+												continue;
+								}
+								if(listen(sockfd[ret-1], SOMAXCONN) == -1)
+								{
 
-		memset(&inaddr, 0, sizeof(struct addrinfo));
-		inaddr.ai_family = AF_UNSPEC;
-		inaddr.ai_socktype = SOCK_STREAM;
-		inaddr.ai_flags = AI_PASSIVE;
-
-		int sfd, ret;
-
-		if( (ret = getaddrinfo(NULL, port, &inaddr, &result)) != 0)
-		{
-				syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__,  __LINE__, strerror(ret));
+												printf("listen %s", gai_strerror(errno));
+												close(sockfd[ret-1]);
+												sockfd[ret-1] = -1;
+												continue;
+								}
+								if(set_nonblocking(sockfd[ret-1]) == -1)
+								{
+												printf("set_nonblocking %s", gai_strerror(errno));
+												close(sockfd[ret-1]);
+												sockfd[ret-1] = -1;
+												continue;
+								}
+								event.data.fd = sockfd[ret-1];
+								event.events = EPOLLIN | EPOLLET;
+								if(epoll_ctl(efd, EPOLL_CTL_ADD, sockfd[ret-1], &event) == -1)
+								{
+												printf("epoll_ctl %s", gai_strerror(errno));
+												close(sockfd[ret-1]);
+												sockfd[ret-1] = -1;
+												continue;
+								}
+								if(ret == 2)
+								{
+												break;
+								}
+				}
+				freeaddrinfo(result);
+				if( (sockfd[0] != -1) && (sockfd[1] != -1) )
+								return 0;
 				return -1;
-		}
-		for(rp = result; rp != NULL; rp = rp->ai_next)
-		{
-				if( (sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) == -1)
-						continue;
-				if(bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
-						break;
-				close(sfd);
-		}
-		if(rp == NULL)
-		{
-				syslog(LOG_ERR, "%s %s %d %s\n", __FILE__, __func__, __LINE__, strerror(errno));
-				return -1;
-		}
-		freeaddrinfo(result);
-		return sfd;
 }
 
-int set_nonblocking(int sfd)
+int set_nonblocking(int socketfd)
 {
-		int flags;
-		if( (flags = fcntl(sfd, F_GETFL, 0)) == -1)
-				return -1;
-		flags |= O_NONBLOCK;
-		if(fcntl(sfd, F_SETFL, flags))
-				return -1;
-		return 0;
+				int flags;
+				if( (flags = fcntl(socketfd, F_GETFL, 0)) == -1)
+								return -1;
+				flags |= O_NONBLOCK;
+				if(fcntl(socketfd, F_SETFL, flags))
+								return -1;
+				return 0;
 }
 
 void * accept_thread(void * data)
 {
-		pthread_t tid = pthread_self();
-		pthread_detach(tid);
-		while(1)
-		{
-				int infd;
-				struct sockaddr inaddr;
-				char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
-				socklen_t len = sizeof inaddr;
-				if( (infd = accept(sfd, &inaddr, &len)) == -1)
+				pthread_t tid = pthread_self();
+				pthread_detach(tid);
+				int fd = (int) data;
+				while(1)
 				{
-						if( (errno != EAGAIN) || (errno != EWOULDBLOCK) )
-								syslog(LOG_ERR, "%s %s %d Thread[0x%lX] accept %s\n",
-												__FILE__, __func__,  __LINE__, tid, strerror(errno));
-						break;
+								int infd;
+								struct sockaddr inaddr;
+								char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+								socklen_t len = sizeof inaddr;
+								if( (infd = accept(fd, &inaddr, &len)) == -1)
+								{
+												if( (errno != EAGAIN) || (errno != EWOULDBLOCK) )
+																syslog(LOG_ERR, "%s %s %d Thread[0x%lX] accept %s\n",
+																								__FILE__, __func__,  __LINE__, tid, strerror(errno));
+												break;
+								}
+								if(getnameinfo(&inaddr, len, hbuf, sizeof hbuf, sbuf, sizeof sbuf, NI_NUMERICHOST | NI_NUMERICSERV) != 0)
+								{
+												syslog(LOG_ERR, "%s %s %d Thread[0x%lX] getnameinfo from FD[%d] %s, closing it\n",
+																				__FILE__, __func__,  __LINE__, tid, infd, strerror(errno));
+												close(infd);
+												break;
+								}
+								if(set_nonblocking(infd) == -1)
+								{
+												syslog(LOG_ERR, "%s %s %d Thread[0x%lX] set nonblocking FD[%d] %s, closing it\n", 
+																				__FILE__, __func__,  __LINE__, tid, infd, strerror(errno));
+												close(infd);
+												break;
+								}
+								event.data.fd = infd;
+								event.events = EPOLLIN | EPOLLET | EPOLLHUP | EPOLLERR | EPOLLPRI | EPOLLRDHUP;
+								if(epoll_ctl(efd, EPOLL_CTL_ADD, infd, &event) == -1)	
+								{
+												syslog(LOG_ERR, "%s %s %d Thread[0x%lX] EPOLL_CTL_ADD FD[%d] %s, closing it\n",
+																				__FILE__, __func__,  __LINE__, tid, infd, strerror(errno));
+												close(infd);
+												break;
+								}
 				}
-				if(getnameinfo(&inaddr, len, hbuf, sizeof hbuf, sbuf, sizeof sbuf, NI_NUMERICHOST | NI_NUMERICSERV) != 0)
-				{
-						syslog(LOG_ERR, "%s %s %d Thread[0x%lX] getnameinfo from FD[%d] %s, closing it\n",
-										__FILE__, __func__,  __LINE__, tid, infd, strerror(errno));
-						close(infd);
-						break;
-				}
-				if(set_nonblocking(infd) == -1)
-				{
-						syslog(LOG_ERR, "%s %s %d Thread[0x%lX] set nonblocking FD[%d] %s, closing it\n", 
-										__FILE__, __func__,  __LINE__, tid, infd, strerror(errno));
-						close(infd);
-						break;
-				}
-				event.data.fd = infd;
-				event.events = EPOLLIN | EPOLLET | EPOLLHUP | EPOLLERR | EPOLLPRI | EPOLLRDHUP;
-				if(epoll_ctl(efd, EPOLL_CTL_ADD, infd, &event) == -1)	
-				{
-						syslog(LOG_ERR, "%s %s %d Thread[0x%lX] EPOLL_CTL_ADD FD[%d] %s, closing it\n",
-										__FILE__, __func__,  __LINE__, tid, infd, strerror(errno));
-						close(infd);
-						break;
-				}
-		}
-		return NULL;
+				return NULL;
 }
 
 
 void * commut_thread(void * data)
 {
-		pthread_t tid = pthread_self();
-		pthread_detach(tid);
-		char * result;
-		int ret;
-		struct list_head * listtmp, *ntmp;
-		struct client * sctmp = NULL;
-		if( (ret =pthread_mutex_lock(&head_mutex)) != 0)
-				syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] lock %s\n",
-								__FILE__, __func__,  __LINE__, tid, strerror(ret));
-		list_for_each_safe(listtmp, ntmp, &head)
-		{
-				sctmp = list_entry(listtmp, struct client, list);
-				if(sctmp->readed)
-						continue;
-				int commutfd = sctmp->cevent.data.fd;
-				while(1)
+				pthread_t tid = pthread_self();
+				pthread_detach(tid);
+				char * result;
+				int ret;
+				struct list_head * listtmp, *ntmp;
+				struct client * sctmp = NULL;
+				if( (ret =pthread_mutex_lock(&head_mutex)) != 0)
+								syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] lock %s\n",
+																__FILE__, __func__,  __LINE__, tid, strerror(ret));
+				list_for_each_safe(listtmp, ntmp, &head)
 				{
-						ssize_t count;
-						char buf[MAXEVENTS];
-						if( (count = read(commutfd, buf, sizeof buf)) == -1)
-						{
-								if(EBADF == errno)
-										pthread_exit(0);
-								if(EAGAIN != errno)
-										syslog(LOG_ERR, "%s %s %d Thread[0x%lX] read FD[%d] %s\n",
-														__FILE__, __func__,  __LINE__, tid, commutfd,  strerror(errno));
-								break;
-						}
-						else if(count == 0)
-						{
-								break;
-						}
-						switch(buf[0])
-						{
-								case '1':
-								case '2':
-										result = get_date_time(tid);
-										break;
-								default:
-										break;
-						}
-						if(write(commutfd, result, strlen(result)) == -1)
-						{
-								syslog(LOG_ERR, "%s %s %d Thread[0x%lX] write FD[%d] %s\n",
-												__FILE__,__func__,  __LINE__, tid, commutfd,  strerror(errno));
-								pthread_exit(0);
-						}
-				} // end while
-				sctmp->readed = 1;
-				sctmp = NULL;
-		} // end list for each
-		if( (ret = pthread_mutex_unlock(&head_mutex)) != 0)
-				syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] unlock %s\n",
-								__FILE__, __func__,  __LINE__, tid, strerror(ret));
-		return NULL;
+								sctmp = list_entry(listtmp, struct client, list);
+								if(sctmp->readed)
+												continue;
+								int commutfd = sctmp->cevent.data.fd;
+								while(1)
+								{
+												ssize_t count;
+												char buf[MAXEVENTS];
+												if( (count = read(commutfd, buf, sizeof buf)) == -1)
+												{
+																if(EBADF == errno)
+																				pthread_exit(0);
+																if(EAGAIN != errno)
+																				syslog(LOG_ERR, "%s %s %d Thread[0x%lX] read FD[%d] %s\n",
+																												__FILE__, __func__,  __LINE__, tid, commutfd,  strerror(errno));
+																break;
+												}
+												else if(count == 0)
+												{
+																break;
+												}
+												switch(buf[0])
+												{
+																case '1':
+																case '2':
+																				result = get_date_time(tid);
+																				break;
+																default:
+																				break;
+												}
+												if(write(commutfd, result, strlen(result)) == -1)
+												{
+																syslog(LOG_ERR, "%s %s %d Thread[0x%lX] write FD[%d] %s\n",
+																								__FILE__,__func__,  __LINE__, tid, commutfd,  strerror(errno));
+																pthread_exit(0);
+												}
+								} // end while
+								sctmp->readed = 1;
+								sctmp = NULL;
+				} // end list for each
+				if( (ret = pthread_mutex_unlock(&head_mutex)) != 0)
+								syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] unlock %s\n",
+																__FILE__, __func__,  __LINE__, tid, strerror(ret));
+				return NULL;
 }
 
 void * free_thread(void * data)
 {
-		pthread_t tid = pthread_self();
-		pthread_detach(tid);
-		struct list_head * listclient, * ntmp;
-		struct client * sctmp = NULL;
-		int ret;
-		while(1)
-		{
-				if(list_empty(&head))
+				pthread_t tid = pthread_self();
+				pthread_detach(tid);
+				struct list_head * listclient, * ntmp;
+				struct client * sctmp = NULL;
+				int ret;
+				while(1)
 				{
-						sleep(1);
-						continue;
+								if(list_empty(&head))
+								{
+												sleep(1);
+												continue;
+								}
+								if( (ret = pthread_mutex_lock(&head_mutex)) != 0)
+								{
+												syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] lock FD[%d] %s\n",__FILE__, __func__,  
+																				__LINE__, tid, sctmp->cevent.data.fd, strerror(ret));
+												continue;
+								}
+								listclient = head.next;
+								sctmp = list_entry( listclient, struct client, list);
+								if(sctmp->readed == 1)
+								{
+												close(sctmp->cevent.data.fd);
+												list_del(&sctmp->list);
+												free(sctmp);
+								}
+								if( (ret = pthread_mutex_unlock(&head_mutex)) != 0)
+												syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] lock FD[%d] %s\n",__FILE__, __func__,  
+																				__LINE__, tid, sctmp->cevent.data.fd, strerror(ret));
 				}
-				if( (ret = pthread_mutex_lock(&head_mutex)) != 0)
-				{
-						syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] lock FD[%d] %s\n",__FILE__, __func__,  
-										__LINE__, tid, sctmp->cevent.data.fd, strerror(ret));
-						continue;
-				}
-				listclient = head.next;
-				sctmp = list_entry( listclient, struct client, list);
-				if(sctmp->readed == 1)
-				{
-						close(sctmp->cevent.data.fd);
-						list_del(&sctmp->list);
-						free(sctmp);
-				}
-				if( (ret = pthread_mutex_unlock(&head_mutex)) != 0)
-						syslog(LOG_DEBUG, "%s %s %d Thread[0x%lX] lock FD[%d] %s\n",__FILE__, __func__,  
-										__LINE__, tid, sctmp->cevent.data.fd, strerror(ret));
-		}
-		return NULL;
+				return NULL;
 }
